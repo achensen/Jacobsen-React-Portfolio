6770aefae4b446f17707c171847cbd796fe4528d
achensen <Erinrjacobsen@gmail.com>
2025-03-07 16:05:55 -0500

    profile and mobile navabr

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './style.css'
+import profileImage from '../../assets/images/Erin Profile.jpg';
 
 const MainPage = () => {
   return (
-    <main>MainPage</main>
-    
+    <main className = "mainContainer">
+      <div className= "aboutSection">
+        <img src= {profileImage} alt= "Erin Jacobsen" className="profileImage"/>
+        <div className="bioText">
+          <h1>About Me</h1>
+          <p>
+            Hi, I'm Erin and I have recently started by education in web develpoment with a Full Stack Course from EDX offered through UCF. Welcome to my portfolio!
+          </p>
+        </div>
+      </div>
+    </main>
+
   )
 }
 
